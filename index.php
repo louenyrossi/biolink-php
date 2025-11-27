aae34b7fae2a037902c46bc621b33f75c4e749f6
Loueny <loueny.rossi@aluno.senai.br>
2025-11-27 08:58:50 -0300

    Criando loop com meus links pessoais

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-    // --- DADOS DO PERFIL (PREENCHA COM SEUS DADOS) ---
+    // --- DADOS DO PERFIL ---
     $nome = "Loueny";
     $bio = "Loira e maromba";
     $imagem = "https://i.pinimg.com/1200x/62/0c/1c/620c1ca9e53342b5d0c69ed2847fa004.jpg";
+
+     $links = [
+        "Instagram" => "https://instagram.com/lou._.rossi",
+        "WhatsApp"  => "https://wa.me/(17)996241799",
+        "Meu Jogo Favorito" => "https://www.playpkxd.com/",
+        "Musica do Momento" => "https://www.youtube.com/watch?v=TdrL3QxjyVw&list=RDTdrL3QxjyVw&start_radio=1"
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +28,21 @@
         <h1> <?php echo $nome; ?> </h1>
         <p> <?php echo $bio; ?> </p>
 
-        <div class="lista-links"></div>
+        <div class="lista-links">
+              <?php
+            foreach ($links as $texto => $url) {
+                $classe_extra = "";
+
+                if ("$texto" == "Instagram") {
+                    $classe_extra = "destaque";
+                }
+
+                echo "<a href='$url' class='btn $classe_extra' target='_blank'>
+                        $texto
+                      </a>";
+            }
+            ?>
+        </div>
     </div>
 </body>
 </html>
